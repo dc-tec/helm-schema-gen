a807ef544da75a98d8cc62f1df314e9d04b8c6d2
Roel de Cort <roel@decort.tech>
2025-04-11 23:24:27 +0200

    feat: Update install scripts
    Signed-off-by: Roel de Cort <roel@decort.tech>

--- scripts/install_version.sh
+++ scripts/install_version.sh
@@ -5,11 +5,17 @@
 ## 2. Updates the version used in the installation
 
 # Get version from first argument or use default
-VERSION=${1:-"1.0.0"}
+VERSION=${1:-"v1.0.0"}
 
 # Update plugin.yaml version
 if command -v yq >/dev/null 2>&1; then
-  yq write -i plugin.yaml version "$VERSION"
+  # Test if yq supports the 'write' command (older versions)
+  if yq --help | grep -q write; then
+    yq write -i plugin.yaml version "$VERSION"
+  else
+    # Newer versions of yq use different syntax
+    yq eval ".version = \"$VERSION\"" -i plugin.yaml
+  fi
 else
   echo "Warning: yq not found. Cannot update plugin.yaml version automatically."
   echo "Please update plugin.yaml version to $VERSION manually."
@@ -23,8 +29,8 @@ fi
 OS=$(uname)
 if [ "$OS" = "Darwin" ]; then
   # macOS requires an empty string with -i
-  sed -i '' "s/VERSION=\${1:-\"[0-9]*\.[0-9]*\.[0-9]*\"}/VERSION=\${1:-\"$VERSION\"}/" "$0"
+  sed -i '' "s/VERSION=\${1:-\"v[0-9]*\.[0-9]*\.[0-9]*\"}/VERSION=\${1:-\"$VERSION\"}/" "$0"
 else
   # Linux version
-  sed -i "s/VERSION=\${1:-\"[0-9]*\.[0-9]*\.[0-9]*\"}/VERSION=\${1:-\"$VERSION\"}/" "$0"
+  sed -i "s/VERSION=\${1:-\"v[0-9]*\.[0-9]*\.[0-9]*\"}/VERSION=\${1:-\"$VERSION\"}/" "$0"
 fi
